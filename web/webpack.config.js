c8a9f2411a88cf3124e2cfeda0e798ecbc2ab818
Daniel Felipe Jaimes Blanco <danielvege6@gmail.com>
2023-08-22 10:39:14 -0500

    feat(build): files

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './lib/main.js',
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, '../docs'),
         filename: 'bundle.js',
     },
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new CopyPlugin([path.resolve(__dirname, "public")]),
+      ],
 };
